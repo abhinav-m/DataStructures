617950e8b4e9d43a85ea525a126b23e26845db54
Abhinav Mishra <abhinav.thinktank@gmail.com>
2016-09-21 23:15:06 +0530

    Fixed static array implementation of queue, and added dynamic array

--- Queues/QueuesDS.java
+++ Queues/QueuesDS.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
 import java.util.Stack;
-/*TBD: Correct Queue implementation The queue implementation here is sort of arbitrary. A better implmentation is possible.*/
+import java.util.LinkedList;
+/*Corrected Queue implementation The queue implementation here is better than the one coded earlier.
+It uses modulus operator to determine new index of the queue array rather than testing for all boundary conditions like
+the previous implementation.
+Basic concept behind this is m%n when m<=n m>=0 will give 0,1,2...n-1 and cycle back to 0(as n%n = 0)
+Also, dividend = divisor*quotient + remainder,when quotient is 0 (always the remainder will be the dividend)
+when dividend = divisor = n remainder = n-n => 0*/
 class CircularArrayQueue
 {
     static final int maxSize = 20;
@@ -13,54 +20,58 @@ class CircularArrayQueue
         size =0;
     }
 
+    public boolean isFullQueue()
+    {
+       return ((rear + 1) % maxSize == front);
+    }
+
     public boolean enQueue(int x) {
-        if (this.isEmpty()) {
-            elements[++rear] = x;
-            front = rear;
-        } else if (this.size == maxSize) {
+
+        if (isFullQueue()){
             System.out.println("\nOverflow.");
             return false;
-        } else if (rear == maxSize - 1) {
-            elements[0] = x;
-            rear = 0;
-        } else
-            elements[++rear] = x;
-
-        size++;
+        } else {
+            //See above explanation.
+            rear = (rear + 1) % maxSize;
+            elements[rear] = x;
+        }
+        if (this.isEmpty()) {
+            front = rear;
+            size++;
+        }
         return true;
     }
 
     public boolean isEmpty()
     {
-        return (size==0);
+        return (front == -1);
     }
 
-    public int deQueue(){
-        int elemDeleted;
-        if(isEmpty())
-        {
+    public int deQueue() {
+        int elemDeleted = 0;
+        if (isEmpty()) {
             System.out.println("\nUnderflow");
             return -1;
-        }
-        else if(front == maxSize-1) {
-            elemDeleted = elements[maxSize-1];
-            front = 0;
-
-        }
-        else {
+        } else {
             elemDeleted = elements[front];
-            ++front;
+            //Only one element left in the queue.
+            if (front == rear) {
+                front = -1;
+                rear = -1;
+            } else
+                //Increment size of queue in the same manner as rear( this also cycles  from 0 to maxSize-1)
+                front = (front + 1) % maxSize;
         }
+
         size--;
-        if(size==0)
-        {
-            front =-1;
-            rear =-1;
-        }
         return elemDeleted;
-
-
     }
+
+    int queueSize()
+    {
+    return(maxSize-front+rear+1)%maxSize;
+    }
+
 
     /*PROBLEM STATEMENT: Reverse a queue using only queue ADT
     * Time complexity: O(n) , this gave me a headache as i realized
@@ -106,17 +117,161 @@ class CircularArrayQueue
             System.out.print(elements[front]);
     }
 }
+//TBD
+class ListQueue
+{
+    LinkedList queueList;
+    int front,rear;
+    int size =;
+    ListQueue()
+    {
+        queueList = new LinkedList();
+        front = -1;
+        rear = -1;
+        size =0;
+    }
+    public  boolean enQueue(int x)
+    {
+        if(queueList.isEmpty())
+
+        queueList.addLast(x);
+    }
+}
+
+class DynamicArrayQueue{
+    int maxSize;
+    int[] elements;
+    int front,rear;
+    int size;
+
+    DynamicArrayQueue(int size)
+    {
+        maxSize = size;
+        elements = new int[maxSize];
+        front = -1;
+        rear = -1;
+        this.size = 0;
+    }
 
 
+    public boolean isFullQueue()
+    {
+        return ((rear + 1) % maxSize == front);
+    }
+
+
+    public int deQueue() {
+        int elemDeleted = 0;
+        if (isEmpty()) {
+        System.out.println("\nUnderflow");
+        return -1;
+        } else {
+        elemDeleted = elements[front];
+        //Only one element left in the queue.
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        } else
+        //Increment size of queue in the same manner as rear( this also cycles  from 0 to maxSize-1)
+        front = (front + 1) % maxSize;
+        }
+
+        size--;
+        return elemDeleted;
+    }
+
+    public boolean enQueue(int x) {
+
+        if (isFullQueue()) {
+            System.out.println("RESIZING QUEUE");
+            resizeQueue();
+        }
+        //See above explanation.
+        rear = (rear + 1) % maxSize;
+        elements[rear] = x;
+
+        if (this.isEmpty()) {
+            front = rear;
+            size++;
+        }
+        return true;
+    }
+
+    public void resizeQueue()
+    {
+        int oldSize = this.maxSize;
+        this.maxSize =this.maxSize*2;
+        //Other way to do this is manually copying each element from the previous array to the element in a newly sized array.
+        elements= Arrays.copyOf(elements,maxSize);
+        //We shift the elements to the back of the queue by adding the old size to their
+        //respective indices , this is done so that the condition for full queue check
+        //ie, front->next = rear doesn't become true when the size of the queue has increased.
+        //this would be a false positive.
+        if(front>rear) {
+            for (int i = 0; i < front; i++)
+                elements[i + oldSize] = elements[i];
+            //rear will be shifted because it's behind front
+            rear = rear + size;
+        }
+    }
+
+        public boolean isEmpty()
+        {
+            return (front == -1);
+        }
+
+
+    public void display()
+    {
+        if(front<rear)
+            for(int i = front;i<=rear;i++) {
+                if(i==front)
+                    System.out.print(elements[i] + "f<-");
+                else if(i==rear)
+                    System.out.print(elements[i] + "r<-");
+                else
+                    System.out.print(elements[i] + "<-");
+            }
+        else if(front>rear) {
+            for (int i = front; i <= maxSize - 1; i++) {
+                if (i == front)
+                    System.out.print(elements[i] + "f<-");
+                else
+                    System.out.print(elements[i] + "<-");
+            }
+
+            for (int i = 0; i <= rear; i++) {
+                if(i==rear)
+                    System.out.print(elements[i] + "r<-");
+                else
+                    System.out.print(elements[i] + "<-");
+            }
+        }
+        else
+            System.out.print(elements[front]);
+    }
+
+    int queueSize()
+    {
+        return(maxSize-front+rear+1)%maxSize;
+    }
+}
 
 
 public class QueuesDS {
 
     public static void main(String[] args) {
 	CircularArrayQueue arrayQueue = new CircularArrayQueue();
+    DynamicArrayQueue  dynArrayQueue = new DynamicArrayQueue(20);
+    for(int i =1;i<=40;i++) {
+    dynArrayQueue.enQueue(i);
+    }
+    dynArrayQueue.display();
     System.out.println("Enqueueing 20");
-    for(int i =1;i<=20;i++)
-    arrayQueue.enQueue(i);
+    for(int i =1;i<=20;i++) {
+        arrayQueue.enQueue(i);
+        System.out.println(arrayQueue.queueSize());
+    }
     arrayQueue.display();
     System.out.println("Reversing the queue\n");
     arrayQueue.revQueue();
