9ec983aebfab1e3001b3230838c4b913f338e47c
Abhinav Mishra <abhinav.mishra2@timesinternet.in>
2020-05-19 17:23:14 +0530

    Added preorder traversal example

--- Trees/Tree.py
+++ Trees/Tree.py
@@ -1,13 +1,26 @@
 class Node:
-    def __init__(s,d):
-        s.l = None
-        s.r = None
-        s.d = d
+    def __init__(self,d,l=None,r=None):
+        self.d = d
+        self.l = None
+        self.r = None
+       
+
+
+    @classmethod
+    def in_order(self,s):
+        if(s):
+            print(s.d)
+        else:
+            return;
+        s.in_order(s.l)
+        s.in_order(s.r)
+
     
-    def print_tree(s):
-        print(s.d)
+    @classmethod
+    def print_tree(self,s):
+        self.in_order(s)
 
 
 
-root = Node(10)
-root.print_tree()
+
+root = Node(10,None,None)
