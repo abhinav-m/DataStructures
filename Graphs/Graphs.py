90027597016d4e3deafa809609d0c0437117acd3
Abhinav Mishra <abhinav.mishra2@timesinternet.in>
2020-06-02 17:42:51 +0530

    Added initial graph implementation using dictionary and lists

---
+++ Graphs/Graphs.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class GraphNode:
+    # Assuming that connections are in the form of tuples
+    # (a,b) means a -> b are connected and so on.
+    def __init__(self,connections,directed=False):
+        self._graph = defaultdict(list)
+        self._directed = directed
+        self.add_connections(connections)
+        
+    def add_connections(self,connections):
+        
+        for vertex1,vertex2 in connections:
+            self.add(vertex1,vertex2)
+    
+    def add(self,vertex1,vertex2):
+        # Add vertex 2 to the connection list of vertex 1
+        self._graph[vertex1].append(vertex2)
+        if not self._directed:
+            # Add vertex 1 back to vertex 2 if graph is not 
+            # directed.
+            self._graph[vertex2].append(vertex1)
