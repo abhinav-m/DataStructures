ae43f45d7ff5a8058d376c25a09a17663666162c
Abhinav Mishra <abhinav.thinktank@gmail.com>
2021-03-30 21:53:50 +0530

    Added disjoint sets start

---
+++ DisjointSetsADT/DisjointSets.py
@@ -0,0 +1,9 @@
+'''
+https://www.ics.uci.edu/~eppstein/PADS/UnionFind.py -> Optimised
+https://code.activestate.com/recipes/577225-union-find/  -> Original
+
+Union find implementation based on weight of sets
+following python standards
+'''
+
+class UnionFind:
